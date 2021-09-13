def5b7cf19dbb3a34b685062e3a6149440a2343b
lphoang <lephuochoang98tcv@gmail.com>
2021-09-13 17:07:49 +0700

    Task management

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+
+@Injectable()
+export class TasksService {
+    private tasks: Task[] = [];
+
+    getAllTasks(): Task[] {
+        return this.tasks;
+    }
+
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+        let tasks = this.getAllTasks();
+
+        if (status) {
+            tasks = tasks.filter((task) => task.status === status)
+        }
+
+        if (search) {
+            tasks = tasks.filter((task) => {
+                if (task.title.includes(search)
+                    || task.description.includes(search)) {
+                    return true;
+                }
+                return false;
+            })
+        }
+
+        return tasks;
+    }
+
+    getTaskById(id: string): Task {
+        return this.tasks.find((task) => task.id === id);
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        };
+        this.tasks.push(task);
+        return task;
+    }
+
+    deleteTaskById(id: string): void {
+        //Solution 1
+        // const task = this.tasks.find((task) => task.id === id);
+        // const index = this.tasks.indexOf(task);
+        // if (index > -1) {
+        //     this.tasks.splice(index, 1);
+        // }
+        //Solution 2
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+    }
+
+    updateTaskStatus(id: string, taskStatus: TaskStatus): Task {
+        const task = this.getTaskById(id);
+        task.status = taskStatus;
+        return task;
+    }
+}
